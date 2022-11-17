e9fbf01d5de5080e1195716569c305a886ec4bb2
Natalia <dgared16@icloud.com>
2022-11-17 15:46:14 +0100

    3

--- Program.cs
+++ Program.cs
@@ -6,6 +6,24 @@ string[] FindArray()
     return Console.ReadLine().Split(" ");
 }
 
+string[] ChooseLess(string[] input, int n)
+{
+    string[] output = new string[CountLess(input, n)];
+
+    for(int i = 0, j = 0; i < input.Length; i++)
+{
+        if(input[i].Length <= n)
+{
+            output[j] = input[i];
+            j++;
+        }
+    }
+
+    return output;
+}
+
+string[] result = ChooseLess(array, 3);
+Console.WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", result)}]");
 
 
 int CountLess(string[] input, int n)

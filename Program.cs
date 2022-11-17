360bd24f16aff43bee402f0b40c00b743fd6e64a
Natalia <dgared16@icloud.com>
2022-11-17 15:45:23 +0100

    2

--- Program.cs
+++ Program.cs
@@ -5,3 +5,18 @@ string[] FindArray()
     Console.WriteLine("Введите массив строк через пробел: ");
     return Console.ReadLine().Split(" ");
 }
+
+
+
+int CountLess(string[] input, int n)
+{
+    int count = 0;
+
+    for(int i = 0; i < input.Length; i++) {
+        if(input[i].Length <= n) {
+            count++;
+        }
+    }
+
+    return count;
+}
